bc4c5af92cad053d604fd8d907340efad98e6743
Silviu-Marius Palade <silviumarius.ovilex@gmail.com>
2026-06-02 16:43:20 +0300

    improving the nav mesh driver

--- Assets/Scripts/Character/Animation/NavMeshAnimationDriver.cs
+++ Assets/Scripts/Character/Animation/NavMeshAnimationDriver.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db589b4bc6293a629eedf91c47b704930a210221a6e18373ea3dcbe5e1edcd3c
-size 773
+oid sha256:d6ac85a2e8a75198dd180f38b159c927dd32adbb56eaf0333a40de6b3a443be2
+size 1344
